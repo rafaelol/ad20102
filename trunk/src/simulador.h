aa1a83856a7820f5a3124c61ec34f57bacd6dcbe
bruno.buss <bruno.buss@07721b43-8a56-793e-f54c-8f9880f99319>
2010-10-25 19:57:07 +0000

    Adicionando documentação das variáveis.

--- trunk/src/simulador.h
+++ trunk/src/simulador.h
@@ -35,13 +35,13 @@ enum TipoSimulacao {BATCH, REPLICATIVO};
 class Simulador
 {
 private:
-priority_queue<Evento, vector<Evento>, greater<Evento> > m_eventos;
+priority_queue<Evento, vector<Evento>, greater<Evento> > m_eventos; /**< Heap com os eventos a serem processador. */
 
-queue<Fregues> m_fila1;
-queue<Fregues> m_fila2;
+queue<Fregues> m_fila1; /**< Fila de espera 1 */
+queue<Fregues> m_fila2; /**< Fila de espera 2 */
 
-TipoFila m_tipo_fila1;
-TipoFila m_tipo_fila2;
+TipoFila m_tipo_fila1; /**< Forma de serviço da fila de espera 1 */
+TipoFila m_tipo_fila2; /**< Forma de serviço da fila de espera 2 */
 
 public:
 
